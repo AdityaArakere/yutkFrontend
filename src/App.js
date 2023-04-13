852fdd7770c7c8799b58a25c5f2e3dab05bff150
AdityaArakere <aditya.arakere@gmail.com>
2023-04-13 11:33:39 +0530

    eighthh

--- src/App.js
+++ src/App.js
@@ -3,21 +3,24 @@ import Landing from "./pages/LandingPage";
 import Login from "./pages/LoginPage";
 import NavBarFunc from "./components/NavbarComponent";
 import Footer from "./components/footerComponent";
+import React, { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    document.title = "Yutk";
+  }, []);
   return (
     <>
-    <BrowserRouter>
-    <NavBarFunc />
-      <Routes>
-        <Route path="/" element={<Landing />}></Route>
-        <Route path="/login" element={<Login />}></Route>
-      </Routes>
-      <Footer />
-    </BrowserRouter>
+      <BrowserRouter>
+        <NavBarFunc />
+        <Routes>
+          <Route path="/" element={<Landing />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </>
   );
 }
 
 export default App;
-
