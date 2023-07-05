8fb34b425d8f2045f772f6bf4d216026734709c1
AdityaArakere <aditya.arakere@gmail.com>
2023-07-05 10:48:53 +0530

    twelvthhh

--- src/pages/Backpacks.js
+++ src/pages/Backpacks.js
@@ -1,26 +1,60 @@
+import React, { useState } from "react";
 import yutkPopularT from "../yutkPopularT.jpg";
 import yutkHoodie2 from "../yutkHoodie2.jpg";
 import bags2 from "../bags2.jpg";
 import bag from "../bag.jpg";
 import bags from "../bags.jpg";
-import './Backpack.css';
+import "./Backpack.css";
+import yutk_bag from "../yutk_bag.mp4";
+import yutk_bag1 from "../yutk_bag1.JPG";
+import yutk_bag2 from "../yutk_bag2.JPG";
+import yutk_bag3 from "../yutk_bag3.JPG";
+import yutk_bag4 from "../yutk_bag4.JPG";
+import yutk_bag5 from "../yutk_bag5.JPG";
+import yutk_bag6 from "../yutk_bag6.JPG";
+import yutk_bag7 from "../yutk_bag7.JPG";
 
 function Backpack() {
+  const imageList = [
+    { type: "video", src: yutk_bag7 },
+    { type: "image", src: yutk_bag1 },
+    { type: "image", src: yutk_bag2 },
+    { type: "image", src: yutk_bag3 },
+    { type: "image", src: yutk_bag4 },
+    { type: "image", src: yutk_bag5 },
+    { type: "image", src: yutk_bag6 },
+  ];
+
+  const [selectedImage, setSelectedImage] = useState(imageList[0]);
+
+  const handleImageSelect = (image) => {
+    setSelectedImage(image);
+  };
+
   return (
     <div className="main-display">
       <div className="left-display">
         <div className="left-pics">
-          <img src={yutkHoodie2} alt="thumbnail" />
-        </div>
-        <div className="left-pics">
-          <img src={yutkHoodie2} alt="thumbnail" />
-        </div>
-        <div className="left-pics">
-          <img src={yutkHoodie2} alt="thumbnail" />
+          {imageList.map((image, index) => (
+            <img
+              key={index}
+              src={image.src}
+              alt={`Image ${index + 1}`}
+              id="left-img"
+              className={selectedImage === image ? "selected" : ""}
+              onClick={() => handleImageSelect(image)}
+            />
+          ))}
         </div>
       </div>
       <div className="right-display">
-      <img src={yutkPopularT} alt="thumbnail" />
+        {selectedImage.type === "video" ? (
+          <video controls loop autoPlay muted width="480" height="504">
+            <source className="vid" src={yutk_bag} type="video/mp4" />
+          </video>
+        ) : (
+          <img src={selectedImage.src} className="right-img" alt="Selected Image" />
+        )}
       </div>
     </div>
   );
--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -8,6 +8,10 @@ function Login() {
   const [usernameLogin, setUsernameLogin] = useState("");
   const [passwordLogin, setPasswordLogin] = useState("");
 
+  const [usernameLogin2, setUsernameLogin2] = useState("");
+
+  const [OTPLogin, setOTPLogin] = useState("");
+
   const [userDisplay, setUserDisplay] = useState("");
 
   const reg = () => {
@@ -29,7 +33,30 @@ function Login() {
       } else {
         setUserDisplay(Response.data[0].username);
       }
-      console.log(Response.data)
+      console.log(Response.data);
+    });
+  };
+
+  const login2 = () => {
+    Axios.post("http://localhost:3003/loginOTP", {
+      username: usernameLogin2,
+    }).then((Response) => {
+      console.log(Response);
+      if (Response.statusText == "OK") {
+        document.getElementById("OTPdetails").style.display = "inline-block";
+        document.getElementById("OTPdetails2").style.display = "inline-block";
+      } else {
+        alert("not works");
+      }
+    });
+  };
+
+  const OTPverify = () => {
+    Axios.post("http://localhost:3003/OTPverify", {
+      OTP: OTPLogin,
+    }).then((Response) => {
+      console.log(Response);
+      alert(Response.data.message);
     });
   };
 
@@ -55,24 +82,50 @@ function Login() {
         <button onClick={reg}>submit</button>
       </div>
 
-      <div className="login"></div>
-      <h1>LOGIN</h1>
-      <label>Username</label>
-      <input
-        type="text"
-        onChange={(e) => {
-          // console.log("in");
-          setUsernameLogin(e.target.value);
-        }}
-      />
-      <label>Password</label>
-      <input
-        type="password"
-        onChange={(e) => {
-          setPasswordLogin(e.target.value);
-        }}
-      />
-      <button onClick={login}>submit</button>
+      <div className="login">
+        <h1>LOGIN</h1>
+        <label>Username</label>
+        <input
+          type="text"
+          onChange={(e) => {
+            // console.log("in");
+            setUsernameLogin(e.target.value);
+          }}
+        />
+        <label>Password</label>
+        <input
+          type="password"
+          onChange={(e) => {
+            setPasswordLogin(e.target.value);
+          }}
+        />
+        <button onClick={login}>submit</button>
+      </div>
+
+      <div className="login2">
+        <h1>LOGIN</h1>
+        <label>phone number</label>
+        <input
+          type="text"
+          onChange={(e) => {
+            setUsernameLogin2(e.target.value);
+          }}
+        />
+        <button onClick={login2}>submit</button>
+
+        <label>OTP</label>
+        <input
+          className="OTPdetails"
+          id="OTPdetails"
+          type="text"
+          onChange={(e) => {
+            setOTPLogin(e.target.value);
+          }}
+        />
+        <button className="OTPdetails2" id="OTPdetails2" onClick={OTPverify}>
+          submit
+        </button>
+      </div>
 
       <h1>{userDisplay}</h1>
     </div>
